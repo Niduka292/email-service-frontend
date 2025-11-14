daac122d344dabeb7fc4e066eac5194b72ef83b5
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-11-14 22:09:54 +0530

    Create Auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import authService from '../services/authService';
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        
+        const currentUser = authService.getCurrentUser();
+        if(currentUser){
+            setUser(currentUser);
+        }
+        setLoading(false);
+    }, []);
+
+    const login = async (credentials) => {
+        try{
+            const userData = await authService.login(credentials);
+            setUser({
+                userId: userData.userId,
+                username: userData.username,
+                email: userData.email,
+            });
+            return {success: true};
+        }catch(error){
+            return {success: false, error: error.message || 'Login falied'};
+        }
+    };
+
+    const signup = async (userData) => {
+        try{
+            await authService.signup(userData);
+            return {success: true};
+        }catch(error){
+            return {success: false, error: error.message || 'Signup falied'};
+        }
+    };
+
+    const logout = () => {
+        authService.logout();
+        setUser(null);
+    };
+
+    const value = {
+        user,
+        login,
+        signup,
+        logout,
+        isAuthenticated : !!user,
+        loading,
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+// Custom hook to use auth context
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if(!context){
+        throw new Error('useAuth must be used within AuthProvider');
+    }
+    return context
+}
